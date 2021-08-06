204c75b510bbd4a58b84b8f1e4fd3847af210aab
liuwen2 <liuwen2@cmss.chinamobile.com>
2021-08-06 09:26:39 +0800

    单元测试

--- tools/test/main.go
+++ tools/test/main.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Println("go test命令会遍历所有的*_test.go文件中符合上述命名规则的函数，" +
 		"然后生成一个临时的main包用于调用相应的测试函数，" +
 		"然后构建并运行、报告测试结果，最后清理测试中生成的临时文件。")
-	fmt.Println("go test\ngo test -v\ngo test -cover")
+	fmt.Println("常用：go test\n查看详情：go test -v\n子测试：test -v -run=Split/simple\n测试覆盖率：go test -cover")
 
 }
