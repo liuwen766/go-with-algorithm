a0c8c52f8cfcfd7671260093da6c175d198ff542
liuwen2 <liuwen2@cmss.chinamobile.com>
2021-08-07 14:06:04 +0800

    算法学习入门

---
+++ leetcode/尚硅谷/1.算法引入/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("数据结构是一门研究算法的学科，只从有了编程语言也就有了数据结构。学好数据结构可以编写\n出更加漂亮，更加有效率的代码。")
+	fmt.Println("算法是程序的灵魂!算法的基石——数据结构!")
+	fmt.Printf("%s=%s+%s", "程序", "数据结构", "算法")
+	fmt.Println()
+	fmt.Println("常见的算法问题：\n1.五子棋\n2.汉诺塔\n3.八皇后\n4.邮差问题\n5.最短路径问题")
+}
