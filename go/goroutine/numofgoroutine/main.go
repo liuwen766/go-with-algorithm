93b9e1516b0bf984071b111e34d2d7729a7d3d00
liuwen2 <liuwen2@cmss.chinamobile.com>
2021-08-13 13:29:18 +0800

    goroutine

--- go/goroutine/numofgoroutine/main.go
+++ go/goroutine/numofgoroutine/main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
 	"sync"
 	"time"
 )
 
 func main() {
-	pool := New(100)
+	pool := New(10)
+	now := time.Now()
 	println(runtime.NumGoroutine())
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 3000; i++ {
 		pool.Add(1)
 		go func() {
 			time.Sleep(time.Second)
-			println(runtime.NumGoroutine())
+			println("liu", runtime.NumGoroutine())
 			pool.Done()
 		}()
 	}
 	pool.Wait()
+	since := time.Since(now)
+	fmt.Println("总共用时：", since)
 	println(runtime.NumGoroutine())
 }
 
